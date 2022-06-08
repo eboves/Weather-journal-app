49b87542604b813d4491829e53cea29016887a69
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-06-08 10:29:30 -0400

    changing branch

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = 3000;
 
+const app = express();
+
+// app.use(bodyParser)
+
+app.use(express.static("website"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, function () {
+  console.log("Server running on port: ", PORT);
+});
