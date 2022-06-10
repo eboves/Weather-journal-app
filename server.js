fc3c7707430652520d88307bcc044c8273f7e3bf
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-06-10 09:31:00 -0400

    Create server

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const cors = require("cors");
 const PORT = 3000;
 
 const app = express();
-
-// app.use(bodyParser)
+// projectData will be the endpoint. every info will be store here
+const projectData = {};
 
 app.use(express.static("website"));
+
+/* Dependencies */
+const bodyParser = require("body-parser");
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
