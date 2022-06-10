645770c98223d315d9bebabc6040a29b5cb49101
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-06-10 09:52:52 -0400

    Client side

--- server.js
+++ server.js
@@ -5,18 +5,15 @@ const PORT = 3000;
 
 const app = express();
 // projectData will be the endpoint. every info will be store here
-const projectData = {};
+// const projectData = {};
 
 app.use(express.static("website"));
 
-/* Dependencies */
-const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Cors for cross origin allowance
-const cors = require("cors");
 app.use(cors());
 
 app.get("/", function (req, res) {
@@ -26,3 +23,13 @@ app.get("/", function (req, res) {
 app.listen(PORT, function () {
   console.log("Server running on port: ", PORT);
 });
+
+const data = [];
+
+app.post("/addMovies", addMovies);
+
+function addMovies(req, res) {
+  data.push(req.body);
+  console.log(data);
+  //   return data;
+}
